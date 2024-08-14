a01ee146123d2500e195b7175bf91df91f07e8d6
Arsh Ilahi <156427743+Arsh249@users.noreply.github.com>
2024-08-14 14:31:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.getElementById('hamburger');
+    const menu = document.getElementById('menu');
+
+    hamburger.addEventListener('click', function() {
+        menu.classList.toggle('show');
+    });
+});
